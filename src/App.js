9358fcec6b8654280b21c88aaaee2643d326a7e0
mi-araf <arafmushfiq@gmail.com>
2022-11-10 16:14:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// TODO: learn from newly after ssc
+
+// Create Component, return multiple HTML froma component
+// now dynamicall add datas
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const nayoks = ["Anwar", "Jafor", "Salman"];
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>I'm a React Person</p>
+                <Person name="Rubel Nayok" age="25" car="3"></Person>
+                <Person name="Jhankar PPPP" age="35" car="7" />
+                <Person name="Sakib Ali" age="5" car="0"></Person>
+                <Person name={nayoks[0]} />
+                <Person name={nayoks[1]} />
+                {/* ways to call data */}
+                <Person />
+                <Person></Person>
+            </header>
+        </div>
+    );
 }
 
+// 1st letter of component will be of capital letter
+// Same in Look, differnt in data
+
+// to dynamically pass the datas from the elements then use props in function
+
+function Person(props) {
+    const personStyle = {
+        border: "3px solid yellow",
+        width: "88%",
+        padding: "5px",
+        margin: "8px",
+    };
+
+    return (
+        <div style={personStyle}>
+            <h1 style={{ border: "7px solid red", padding: "10px" }}>
+                Name: {props.name} & age: {props.age}
+            </h1>
+            <h3>Hero Person car: {props.car}</h3>
+        </div>
+    );
+}
 export default App;
